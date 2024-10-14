92969079856311b352aa9d87588fd7a776e62b02
Felipe Castillo A. <54959006+fcast001@users.noreply.github.com>
2024-10-14 22:56:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,18 @@ with open('Model/model.pkl', 'rb') as f:
 # Crear la interfaz de Streamlit
 st.title('Real Estate Price Prediction')
 
-# Crear entradas para las características
-distance_to_mrt = st.number_input('Distance to MRT Station (meters)', min_value=0.0, step=1.0)
-num_convenience_stores = st.number_input('Number of Convenience Stores', min_value=0, step=1)
-latitude = st.number_input('Latitude', min_value=0.0, step=0.01)
-longitude = st.number_input('Longitude', min_value=0.0, step=0.01)
+# Crear dos columnas para las entradas
+col1, col2 = st.columns(2)
+
+# Entradas en la primera columna
+with col1:
+    distance_to_mrt = st.number_input('Distance to MRT Station (meters)', min_value=0.0, step=1.0)
+    num_convenience_stores = st.number_input('Number of Convenience Stores', min_value=0, step=1)
+
+# Entradas en la segunda columna
+with col2:
+    latitude = st.number_input('Latitude', min_value=0.0, step=0.01)
+    longitude = st.number_input('Longitude', min_value=0.0, step=0.01)
 
 # Botón para hacer la predicción
 if st.button('Predict'):
